e319ebf2e946e554ae19a35289b72ab77936cbca
iHeroN <iherondev@gmail.com>
2022-09-05 19:42:29 +0200

    First commit

---
+++ contracts/VaultWrapper.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IVault.sol";
+
+contract VaultWrapper is Ownable {
+    IVault public vault;
+    address public treasury; //Default address for the ones that deposit without a referral code
+    mapping(address => address) referrals;
+
+    event DepositReferral(
+        address account,
+        uint256 sizeDelta,
+        address referrer
+    );
+
+    event WithdrawReferral(
+        address account,
+        uint256 sizeDelta,
+        address referrer
+    );
+
+    event ReferrerChanged(
+        address account,
+        address oldReferrer,
+        address newReferrer
+    );
+
+    function deposit(uint256 amount, address referrer) external returns (uint256) {
+        address recipient = msg.sender;
+        address prevReferrer = referrals[recipient];
+
+
+        if(prevReferrer == 0x0) {
+            referrals[recipient] = (referrer == 0x0)?treasury:referrer;
+        }else if(prevReferrer != referrer && referrer != 0) {
+            emit ReferrerChanged(recipient, prevReferrer, referrer);
+            //TODO decrease position of old referrer and increase new referrer?
+            // uint256 amountAlreadyDeposited = vault.balanceOf(recipient);
+            // emit WithdrawReferral(recipient, amountAlreadyDeposited, prevReferrer);
+            // emit DepositReferral(recipient, amountAlreadyDeposited, referrer);
+        }
+
+        emit DepositReferral(recipient, amount, referrals[recipient]);
+        return vault.deposit(amount, recipient);
+    }
+    
+    
+    function withdraw(uint256 amount, uint256 maxLoss) external returns (uint256) {
+        if(referrals[msg.sender] != 0x0) {
+            emit WithdrawReferral(msg.sender, amount, referrals[recipient]);
+        }
+        return vault.withdraw(amount, msg.sender, maxLoss);
+    }
+
+    function setVaultAddress(address newAddress) external onlyOwner {
+        vault = IVault(newAddress);
+    } 
+
+    function setTreasury(address newAddress) external onlyOwner {
+        treasury = newAddress;
+    } 
+}
---
+++ contracts/interfaces/IERC20Extended.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0
+// Feel free to change the license, but this is what we use
+
+// Feel free to change this version of Solidity. We support >=0.6.0 <0.7.0;
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+// These are the core Yearn libraries
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+interface IERC20Extended is IERC20 {
+    function decimals() external view returns (uint8);
+}

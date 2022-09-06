2f468c101cb0fb392b7a9f00cfca0a7cee80a742
iHeroN <iherondev@gmail.com>
2022-09-06 15:32:02 +0200

    Update VaultWrapper

--- contracts/VaultWrapper.sol
+++ contracts/VaultWrapper.sol
@@ -2,61 +2,49 @@
 
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IVault.sol";
 
 contract VaultWrapper is Ownable {
     IVault public vault;
     address public treasury; //Default address for the ones that deposit without a referral code
+    address public token;
+
     mapping(address => address) referrals;
 
-    event DepositReferral(
+    constructor(address _vault, address _treasury) public {
+        vault = IVault(_vault);
+        token = vault.token();
+        treasury = _treasury;
+    }
+
+    event ReferrerSet(
         address account,
-        uint256 sizeDelta,
         address referrer
     );
 
-    event WithdrawReferral(
-        address account,
-        uint256 sizeDelta,
-        address referrer
-    );
-
-    event ReferrerChanged(
-        address account,
-        address oldReferrer,
-        address newReferrer
-    );
 
     function deposit(uint256 amount, address referrer) external returns (uint256) {
         address recipient = msg.sender;
-        address prevReferrer = referrals[recipient];
 
+        IERC20(token).transferFrom(recipient, address(this), amount);
 
-        if(prevReferrer == address(0)) {
+        if(referrals[recipient] == address(0)) {
             referrals[recipient] = (referrer == address(0))?treasury:referrer;
-        }else if(prevReferrer != referrer && referrer != address(0)) {
-            emit ReferrerChanged(recipient, prevReferrer, referrer);
-            //TODO decrease position of old referrer and increase new referrer?
-            // uint256 amountAlreadyDeposited = vault.balanceOf(recipient);
-            // emit WithdrawReferral(recipient, amountAlreadyDeposited, prevReferrer);
-            // emit DepositReferral(recipient, amountAlreadyDeposited, referrer);
+            emit ReferrerSet(recipient, referrals[recipient]);
         }
 
-        emit DepositReferral(recipient, amount, referrals[recipient]);
         return vault.deposit(amount, recipient);
-    }
-    
-    
-    function withdraw(uint256 amount, uint256 maxLoss) external returns (uint256) {
-        if(referrals[msg.sender] != address(0)) {
-            emit WithdrawReferral(msg.sender, amount, referrals[msg.sender]);
-        }
-        return vault.withdraw(amount, msg.sender, maxLoss);
     }
 
     function setVaultAddress(address newAddress) external onlyOwner {
         vault = IVault(newAddress);
+    } 
+
+    function setToken(address newAddress) external onlyOwner {
+        token = newAddress;
     } 
 
     function setTreasury(address newAddress) external onlyOwner {
